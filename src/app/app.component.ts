d62b761ac351d5c3f8b5f905a9df7443a8b2e217
Ajo John <ajojohn555@gmail.com>
2017-07-30 16:25:41 +0530

    Corrected the ng2-codemirror error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import 'ng2-codemirror';
+import 'codemirror/mode/go/go';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @Input() config: any;
+  @Input() content: string;
   title = 'app';
+  constructor() {
+    this.config = { lineNumbers: true, mode: 'text/x-go' };
+  }
+  ngOnInit() {
+  }
 }
+
